4ef05b53db42cfa14d98a87708cd81684c84aeae
agusrivan <agusrivan.faz@gmail.com>
2023-11-07 09:53:50 +0700

    menambahkan scrool smooth dan tombol to-top

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const NavFixed = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > NavFixed) {
     header.classList.add("navbar-fixed");
+    toTop.classList.add("flex");
+    toTop.classList.remove("hidden");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -17,4 +22,12 @@ const navMenu = document.querySelector("#nav-menu");
 humberger.addEventListener("click", function () {
   humberger.classList.toggle("humberger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// Klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != humberger && e.target != navMenu) {
+    humberger.classList.remove("humberger-active");
+    navMenu.classList.add("hidden");
+  }
 });
